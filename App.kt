ca2ee1e5aba19f180c17f6c6862ec1fa8d3a271b
Artem Mukminov <53013967+artemmukminov@users.noreply.github.com>
2022-08-15 11:19:31 +0400

    Update App.kt

--- App.kt
+++ App.kt
@@ -1,8 +1,3 @@
-//TODO Нужно добавить:
-// - Ввод пользвателя
-// - Arabit to Roman
-// - Вынести функциональность в отедльные функции
-
 fun main() {
     val romanToArabicMap: Map<Char, Int> = mapOf('I' to 1, 'V' to 5, 'X' to 10, 'L' to 50, 'C' to 100, 'D' to 500,
         'M' to 1000)
